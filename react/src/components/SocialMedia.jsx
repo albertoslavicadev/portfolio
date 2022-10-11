4e1d767381badd0f5ce7ddff619b3e25ea8f2e9f
hathateth <albertoslavica19@gmail.com>
2022-10-11 09:58:16 +0200

    done

---
+++ react/src/components/SocialMedia.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <a href="https://twitter.com/shakleDev" target="_blank" rel="noreferrer"><BsTwitter /></a>
+    </div>
+    <div>
+      <a href="https://www.linkedin.com/in/albertoslavica/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+    </div>
+    <div>
+      <a href="https://github.com/hathateth" target="_blank" rel="noreferrer"><BsGithub /></a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
